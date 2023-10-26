f13c09dc32dea787cbc82560e01f1b4c46fbdc51
Melderika <amelderika72@gmail.com>
2023-10-26 11:41:33 +0700

    tambah

--- index.js
+++ index.js
@@ -1,26 +1,111 @@
 const express = require('express')
-const app = express()
+const { Client } = require("pg");
+
+const app = express();
+app.use(express.json());
+
 const port = 5151
 
- // representasi data dari database
- const data_users = [
-    { id: 1, name: "Kamelia", alamat: "Bandung" },
-    { id: 2, name: "Syalamah", alamat: "Bandung" },
-    { id: 3, name: "Dini", alamat: "Jogja" },
-    { id: 4, name: "Saskia", alamat: "Sumedang" },
-];
+const client = new Client ({
+    user: "postgres",
+    host: "localhost",
+    database: "book_store_api",
+    password: "ROOT",
+    port: 5432,
+})
 
-app.get("/nama", (req, res) => {
-    //mendapatkan data dari database
-    const data = data_users;
+client.connect()
+    .then(() => {
+        console.log("Terhubung ke database postgreSQL");
+    })
+    .catch((error) => {
+        console.error("Gagal terhubung ke database:");
+    });
 
-    //memberikan respon json data
-    let result = {
-        status: 200,
-        data: data,
-    };
+app.get('/user',(req, res) => {
+    //akses database
+    client.connect()
+    .then(() => {
+        console.log("Terhubung ke database postgreSQL");
+    })
+    .catch((error) => {
+        console.error("Gagal terhubung ke database:");
+    });
 
-    res.json(result);
+    //ambil data
+    let data;
+    client.query("SELECT id, nama, alamat FROM users;", (req, result) => {
+        res.json(result.rows);
+    });
 });
 
-app.listen(port,() => console.log(`Server running on port ${port}`))
+app.get('/user/:id', async (req, res) => {
+
+    let id = parseInt(req.params.id);
+
+    try {
+        const query = "select * from users where id = $1";
+        const {rows} = await client.query(query, [id]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                message: "Data not found"
+            });
+        } else {
+            res.json({
+                data: rows[0],
+            });
+        }
+    } catch (error) {}
+      return res.status(404).json({
+        message: "Data not found"
+      });
+});
+
+app.post('/user', async (req, res) => {
+    let data = req.body;
+
+    const query = "INSERT INTO public.users(id, nama, alamat)VALUES ($1, $2, $3);";
+
+    client.query(query, [data.id, data.nama, data.alamat], (err, result) => {
+        if (err) {
+            console.error("Error executing INSERT query:", err);
+            return res.status(500).json({error: "Terjadi kesalahan" + err});
+        } else {
+            res.json({
+                message: "Data berhasil dimasukkan"
+            })
+        }
+    });
+
+});
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+ // representasi data dari database
+//  const data_users = [
+//     { id: 1, name: "Kamelia", alamat: "Bandung" },
+//     { id: 2, name: "Syalamah", alamat: "Bandung" },
+//     { id: 3, name: "Dini", alamat: "Jogja" },
+//     { id: 4, name: "Saskia", alamat: "Sumedang" },
+// ];
+
+// app.get("/users/:id", (req, res) => {
+//     // get data dari parameter
+//     let id = parseInt(req.params.id);
+
+//     // get data dari database
+//     let result;
+//     const user = data_users.find((user) => user.id === id);
+//     if (user) {
+//         result = {
+//         status: 200,
+//         data: user,
+//         };
+//     } else {
+//         res.status(404).json({ error: "User not found" });
+//     }
+//     res.json(result);
+// });
+
